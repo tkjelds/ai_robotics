e184dfd76ac192fdd26ee51bd3fbef89bc988eb1
tkjelds <tkjelds385@gmail.com>
2026-02-19 15:45:54 +0100

    added plots for the experiments

--- assignment_1/experiment_visualisation.py
+++ assignment_1/experiment_visualisation.py
@@ -11,8 +11,50 @@ json_path = BASE_DIR / "experiment_data.json"
 with open(json_path, 'r') as f:
     experiment_data = json.load(f)
 
+def save_plot(plot,filename):
+    filename = filename.replace(" ", "_") + ".png"
+    path = BASE_DIR / "visualisations"
+    plot.savefig(path / filename)
 
+### DATA FRAME CREATION ###
 df_exploration = pd.DataFrame(experiment_data["exploration"])
 
-sns.heatmap(df_exploration, annot=True,)
+df_edgeDetection = pd.DataFrame(
+    [(angle, value) for angle, values in experiment_data["edgeDetection"].items() for value in values],
+    columns=["Entrance Degree", "Distance (mm)"]
+)
+
+df_objectDetection = pd.DataFrame(
+    [(object, distance) for object, distances in experiment_data["objectDetection"].items() for distance in distances],
+    columns=["Object", "Distance (mm)"]
+)
+
+df_cornerEvacuation = pd.DataFrame(experiment_data["cornerEvacuation"], columns=["Corner Evacuation Time (s)"])
+
+
+### PLOTS
+
+plt.figure(figsize=(8,6))
+
+plt.title("Exploration Data Heatmap")
+sns.heatmap(df_exploration, annot=True, )
+save_plot(plt, "Exploration Data Heatmap")
+plt.show()
+
+sns.boxplot(data=df_edgeDetection, x="Entrance Degree", y="Distance (mm)")
+sns.swarmplot(data=df_edgeDetection, x="Entrance Degree", y="Distance (mm)", color="black")
+plt.title("Nearest Contact Distance vs Entrance Degree")
+save_plot(plt, "Nearest Contact Distance vs Entrance Degree")
+plt.show()
+
+plt.title("Object Detection Distances")
+sns.boxplot(data=df_objectDetection, x="Object", y="Distance (mm)")
+sns.swarmplot(data=df_objectDetection, x="Object", y="Distance (mm)", color="black")
+save_plot(plt, "Object Detection Distances")
+plt.show()
+plt.title("Distribution of Corner Evacuation Time")
+
+sns.boxplot(y=df_cornerEvacuation["Corner Evacuation Time (s)"])
+sns.stripplot(y=df_cornerEvacuation["Corner Evacuation Time (s)"], color="black")
+save_plot(plt, "Corner Evacuation Time Distribution")
 plt.show()
